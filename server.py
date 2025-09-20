866c52568c1e1f38ee5c30f795fc78bdeac1cf31
Noah Raimbaud <raimbaudnoah@gmail.com>
2025-09-20 12:32:17 +0200

    Implemented the full fake news functionality

--- server.py
+++ server.py
@@ -29,20 +29,49 @@ class ProxyServer:
         self.serverSocket.close()
         exit(0)
 
-    def change_content_length(self, data):
-
+    def change_content_length(self, data, difference):
         start = "Content-Length: "
-        # Find the index of the start substring
+        # Find the index of the start and end substring
         idx1 = data.find(start)
-
-        # Find the index of the end substring, starting after the start substring
         idx2 = data.find("\r", idx1 + len(start))
-
+        # Find the original value
         number = int(data[idx1 + len(start):idx2])
-        print("Nombre : ", number)
-        return data.replace(str(number), str(number+1))
+        
+        return data.replace(str(number), str(number + difference), 1)
 
+    def replace_link(self, data, old="./smiley.jpg", new="https://i.redd.it/cgefug8s28881.jpg"):
+        # count the occurences of old link
+        nb_links = data.count(old)
+        # calculate the difference of length between old and new in utf-8
+        char_diff = len(bytes(new, 'utf-8')) - len(bytes(old, 'utf-8'))
+
+        if (nb_links > 0):
+            # replace old by new
+            data = data.replace(old, new)
+            # update the content_length field
+            data = self.change_content_length(data, char_diff*nb_links)
+        return data
     
+    def replace_word(self, data, old=" Stockholm", new=" Linköping"):
+        # count the occurences of old word
+        nb_words = data.count(old)
+        # calculate the difference of length between old and new in utf-8
+        char_diff = len(bytes(new, 'utf-8')) - len(bytes(old, 'utf-8'))
+        
+        if (nb_words > 0):
+            # replace old by new
+            data = data.replace(old, new)
+            # update the content_length field
+            data = self.change_content_length(data, char_diff*nb_words)
+        return data
+    
+    def apply_fake_news(self, data):
+        # takes an http packet as input and outputs this packet with fake news
+        data = self.replace_word(data, " Stockholm", " Linköping")
+        data = self.replace_word(data, "./smiley.jpg", "https://i.redd.it/cgefug8s28881.jpg")
+        data = self.replace_word(data, "Smiley", "Siphano")
+        return data
+
 
     def proxy_thread(self, clientSocket):
         request = clientSocket.recv(self.config['MAX_REQUEST_LEN'])
@@ -51,9 +80,6 @@ class ProxyServer:
         # Parse URL
         first_line = request.decode().split('\n')[0]
         url = first_line.split(' ')[1]
-        if "smiley.jpg" in url:
-            request.url = "https://i.redd.it/cgefug8s28881.jpg"
-
         http_pos = url.find("://")
         temp = url[(http_pos+3):] if http_pos != -1 else url
 
@@ -76,19 +102,17 @@ class ProxyServer:
 
         while True:
             data = s.recv(self.config['MAX_REQUEST_LEN'])
-            if len(data) > 0 and "jpg" not in url:
+    
+            if len(data) > 0 and "html" in url:
                 # Converts the utf-8 data into string
                 data = data.decode('utf-8', 'ignore')
-                print("\nAnswer : \n", data)
-
-                if data.find("Stockholm") != -1:
-                    data = data.replace("Stockholm","Linköping")
-                    x = data.find("Stockholm")
-                    data = self.change_content_length(data)
-
+                data = self.apply_fake_news(data)
+                #data = self.replace_word(data)
+                #data = self.replace_link(data)
                 data = bytes(data,  "utf-8")
+              
                 clientSocket.send(data)
-            elif "jpg" in url :
+            elif len(data) > 0:
                 clientSocket.send(data)
             else:
                 break
